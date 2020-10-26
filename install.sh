029e8e87e29dabe156baeaf5cbf098a34bc364f8
AgentRG <ron.gordin@gmail.com>
2020-10-25 20:07:19 -0700

    New version of the script. Rather than having a one giant function, it's now divided into individual functions.

--- install.sh
+++ install.sh
@@ -4,132 +4,169 @@ NONE='\033[00m'
 PURPLE='\033[01;35m'
 RED='\033[0;31m'
 
+create_temporary_downloads_folder() {
+  echo -e "${PURPLE}\t(1/1) Creating temporary downloads folder${NONE}"
+  mkdir ~/swtor_tmp
+}
+
+install_package_wget() {
+  echo -e "${PURPLE}\t(1/5) Installing wget${NONE}"
+  brew install wget
+}
+
+install_package_cask() {
+  echo -e "${PURPLE}\t(2/5) Installing cask${NONE}"
+  brew install cask
+}
+
+install_package_xquartz() {
+  echo -e "${PURPLE}\t(3/5) Installing XQuartz (Might take a while)${NONE}"
+  brew cask install xquartz
+}
+
+install_package_wine() {
+  echo -e "${PURPLE}\t(4/5) Installing Wine${NONE}"
+  brew cask install --no-quarantine wine-stable
+}
+
+install_package_winetricks() {
+  echo -e "${PURPLE}\t(5/5) Installing Winetricks${NONE}"
+  brew install winetricks
+}
+
+create_swtor_prefix() {
+  echo -e "${PURPLE}\t(1/1) Creating "SWTOR On Mac" Wine prefix${NONE}"
+  WINEARCH=win64 WINEPREFIX=~/"SWTOR On Mac" wine wineboot >/dev/null 2>&1
+}
+
+install_dll_vcrun2008() {
+  echo -e "${PURPLE}\t(1/3) Installing vcrun2008${NONE}"
+  env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q vcrun2008 >/dev/null 2>&1
+}
+
+install_dll_crypt32() {
+  echo -e "${PURPLE}\t(2/3) Installing crypt32${NONE}"
+  env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q crypt32 >/dev/null 2>&1
+}
+
+install_dll_d3dx9_36() {
+  echo -e "${PURPLE}\t(3/3) Installing d3dx9_36${NONE}"
+  env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q d3dx9_36 >/dev/null 2>&1
+}
+
+set_vram() {
+  echo -e "${PURPLE}\t(1/2) Setting prefix VRAM to 512${NONE}"
+  env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q videomemorysize=512 >/dev/null 2>&1
+}
+
+switch_windows_version() {
+  echo -e "${PURPLE}\t(2/2) Switching Windows version to Windows 10${NONE}"
+  env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q win10 >/dev/null 2>&1
+}
+
+download_swtor_fix() {
+  echo -e "${PURPLE}\t(1/3) Downloading swtor_fix.exe from https://github.com/AgentRG/swtor_fix/${NONE}"
+  wget https://github.com/AgentRG/swtor_fix/raw/master/swtor_fix.exe -q
+}
+
+download_swtor() {
+  echo -e "${PURPLE}\t(2/3) Downloading SWTOR_setup.exe from http://www.swtor.com/download${NONE}"
+  wget -q -O SWTOR_setup.exe http://www.swtor.com/download -q
+}
+
+download_swtor_icon() {
+  echo -e "${PURPLE}\t(3/3) Downloading swtor_logo.icns from https://github.com/AgentRG/swtor_on_mac"
+  wget -q -O swtor_icon.icns https://github.com/AgentRG/swtor_on_mac/blob/master/swtor_logo.icns?raw=true
+}
+
+move_swtor_fix() {
+  echo -e "${PURPLE}\t(1/3) Moving swtor_fix.exe to prefix folder${NONE}"
+  mv ~/swtor_tmp/swtor_fix.exe ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
+}
+
+move_swtor_setup() {
+  echo -e "${PURPLE}\t(2/3) Moving SWTOR_setup.exe to prefix folder${NONE}"
+  mv ~/swtor_tmp/SWTOR_setup.exe ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
+}
+
+move_swtor_icon() {
+  echo -e "${PURPLE}\t(3/3) Moving swtor_icon.icns to prefix folder${NONE}"
+  mv ~/swtor_tmp/swtor_icon.icns ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
+}
+
+delete_temporary_downloads_folder () {
+  echo -e "${PURPLE}\t(1/1) Deleting temporary downloads folder ${NONE}"
+  rm -r ~/swtor_tmp/
+}
+
+launch_swtor () {
+  echo -e "${PURPLE}\tLaunching SWTOR_setup.exe...${NONE}"
+  WINEPREFIX=~/"SWTOR On Mac" wine ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/SWTOR_setup.exe >/dev/null 2>&1
+}
+
 # Main function that installs Homebrew packages and SWTOR
-install () {
-	sleep 1
+install() {
 
-	echo
-	echo -e "${PURPLE}     Step 1: Create temporary downloads folder${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
+  echo -e "${PURPLE}\tStep 1: Create temporary downloads folder${NONE}"
+  echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
 
-	# Create temporary downloads folder for executables and icon
-	echo -e "${PURPLE}     (1/1) Creating temporary downloads folder${NONE}"
-	sleep 1
-	mkdir ~/swtor_tmp
+  create_temporary_downloads_folder
 
-	echo
-	echo -e "${PURPLE}     Step 2: Install Homebrew packages${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
+  echo -e "${PURPLE}\tStep 2: Install Homebrew packages${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
 
-	# Install Wget
-	echo -e "${PURPLE}     (1/5) Installing wget${NONE}"
-	brew install wget &&
+  install_package_wget
+  install_package_cask
+  install_package_xquartz
+  install_package_wine
+  install_package_winetricks
 
-	# Install Cask
-	echo
-	echo -e "${PURPLE}     (2/5) Installing cask${NONE}"
-	brew install cask &&
+  echo -e "${PURPLE}\tStep 3: Create custom Wine prefix${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
 
-	# Install XQuartz
-	echo
-	echo -e "${PURPLE}     (3/5) Installing XQuartz (Might take a while)${NONE}"
-	brew cask install xquartz &&
+  create_swtor_prefix
 
-	# Install Wine
-	echo
-	echo -e "${PURPLE}     (4/5) Installing Wine${NONE}"
-	brew cask install --no-quarantine wine-stable &&
+  echo -e "${PURPLE}\tStep 4: Install DLLs to prefix${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
 
-	# Install Winetricks
-	echo
-	echo -e "${PURPLE}     (5/5) Installing Winetricks${NONE}"
-	brew install winetricks &&
+  install_dll_vcrun2008
+  install_dll_crypt32
+  install_dll_d3dx9_36
 
-	echo
-	echo -e "${PURPLE}     Step 3: Create custom Wine prefix${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
+  echo -e "${PURPLE}\tStep 5: Change prefix settings${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
 
-	# Create a new Wine prefix titled "SWTOR On Mac"
-	echo -e "${PURPLE}     (1/1) Creating "SWTOR On Mac" Wine prefix${NONE}"
-	WINEARCH=win64 WINEPREFIX=~/"SWTOR On Mac" wine wineboot> /dev/null 2>&1 &&
+  set_vram
+  switch_windows_version
 
-	echo
-	echo -e "${PURPLE}     Step 4: Install DLLs to prefix${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
+  echo -e "${PURPLE}\tStep 6: Download executables and icon${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
 
-	# Install vcrun2008
-	echo -e "${PURPLE}     (1/3) Installing vcrun2008${NONE}"
-	env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q vcrun2008> /dev/null 2>&1  &&
+  cd ~/swtor_tmp/ || exit
+  download_swtor_fix
+  download_swtor
+  download_swtor_icon
+  cd ~/ || exit
 
-	# Install crypt32
-	echo -e "${PURPLE}     (2/3) Installing crypt32${NONE}"
-	env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q crypt32> /dev/null 2>&1  &&
+  echo -e "${PURPLE}\tStep 7: Move executables and icon and move to prefix folder${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
 
-	# Install d3dx9_36
-	echo -e "${PURPLE}     (3/3) Installing d3dx9_36${NONE}"
-	env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q d3dx9_36> /dev/null 2>&1  &&
+  move_swtor_fix
+  move_swtor_setup
+  move_swtor_icon
 
-	echo
-	echo -e "${PURPLE}     Step 5: Change prefix settings${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ${NONE}"
+  echo -e "${PURPLE}\tStep 8: Delete temporary downloads folder${NONE}"
+  echo -e "${PURPLE}\t‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
 
-	# Set VRAM to 512
-	echo -e "${PURPLE}     (1/2) Setting prefix VRAM to 512${NONE}"
-	env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q videomemorysize=512> /dev/null 2>&1 &&
+  delete_temporary_downloads_folder
 
-	# Switch Windows version to 10
-	echo -e "${PURPLE}     (2/2) Switching Windows version to Windows 10${NONE}"
-	env WINEPREFIX=~/"SWTOR On Mac" sh winetricks -q win10> /dev/null 2>&1 &&
+  echo -e "${PURPLE}\tSWTOR On Mac Installation Finished Successfully!${NONE}"
 
-	echo
-	echo -e "${PURPLE}     Step 6: Download executables and icon${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
-
-	# Download SWTOR_setup.exe, swtor_fix.exe and swtor_icon.icns
-	cd ~/swtor_tmp/ || exit
-	echo -e "${PURPLE}     (1/3) Downloading swtor_fix.exe from https://github.com/AgentRG/swtor_fix/${NONE}"
-	wget https://github.com/AgentRG/swtor_fix/raw/master/swtor_fix.exe -q &&
-	echo -e "${PURPLE}     (2/3) Downloading SWTOR_setup.exe from http://www.swtor.com/download${NONE}"
-	wget -q -O SWTOR_setup.exe http://www.swtor.com/download -q &&
-	echo -e "${PURPLE}     (3/3) Downloading swtor_logo.icns from https://github.com/AgentRG/swtor_on_mac"
-	wget -q -O swtor_icon.icns https://github.com/AgentRG/swtor_on_mac/blob/master/swtor_logo.icns?raw=true
-	cd ~/ || exit
-	sleep 1
-
-	echo
-	echo -e "${PURPLE}     Step 7: Move executables and icon and move to prefix folder${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
-
-	# Move "SWTOR_Setup.exe, swtor_fix.exe and swtor_icon.icns to Program Files (x86) folder"
-	echo -e "${PURPLE}     (1/3) Moving swtor_fix.exe to prefix folder${NONE}"
-	mv ~/swtor_tmp/swtor_fix.exe ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
-	echo -e "${PURPLE}     (2/3) Moving SWTOR_setup.exe to prefix folder${NONE}"
-	mv ~/swtor_tmp/SWTOR_setup.exe ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
-	echo -e "${PURPLE}     (3/3) Moving swtor_icon.icns to prefix folder${NONE}"
-	mv ~/swtor_tmp/swtor_icon.icns ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/
-	sleep 1
-
-	echo
-	echo -e "${PURPLE}     Step 8: Delete temporary downloads folder${NONE}"
-	echo -e "${PURPLE}     ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾${NONE}"
-
-	# Delete temporary downloads folder
-	echo -e "${PURPLE}     (1/1) Deleting temporary downloads folder ${NONE}"
-	rm -r ~/swtor_tmp/
-	sleep 1
-
-	echo
-	echo -e "${PURPLE}     SWTOR On Mac Installation Finished Successfully!${NONE}"
-
-	# Launch SWTOR_Setup.exe
-	sleep 2
-	echo
-	echo -e "${PURPLE}     Launching SWTOR_setup.exe...${NONE}"
-	WINEPREFIX=~/"SWTOR On Mac" wine ~/SWTOR\ On\ Mac/drive_c/Program\ Files\ \(x86\)/SWTOR_setup.exe > /dev/null 2>&1
+  launch_swtor
 }
 
 echo
-echo -e "${PURPLE}     AgentRG's SWTOR On Mac ${NONE}"
+echo -e "${PURPLE}\tAgentRG's SWTOR On Mac\n${NONE}"
 
 tools_version=$(xcode-select -p)
 tools_installed="/Library/Developer/CommandLineTools"
@@ -137,11 +174,11 @@ tools_installed="/Library/Developer/CommandLineTools"
 # Check if Command Line Tools are installed followed by if Homebrew is installed
 # If either isn't installed, the script will quit
 if [ "$tools_version" = "$tools_installed" ]; then
-        if [[ $(command -v brew) == "" ]]; then
-                echo -e "${RED}     ERROR: Homebrew not installed. Exiting.${NONE}"
-        else
-                install
-        fi
+  if [[ $(command -v brew) == "" ]]; then
+    echo -e "${RED}\tERROR: Homebrew not installed. Exiting.${NONE}"
+  else
+    install
+  fi
 else
-        echo -e "${RED}     ERROR: Command Line Tools not installed. Exiting.${NONE}"
+  echo -e "${RED}\tERROR: Command Line Tools not installed. Exiting.${NONE}"
 fi
