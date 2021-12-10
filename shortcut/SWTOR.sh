9bef270313800cb362e6f4acc8d0db237c4ee87a
agentrg <ron.gordin@gmail.com>
2021-12-09 19:37:06 -0800

    Applied macOS version determination from main script to the shortcut as well.

--- shortcut/SWTOR.sh
+++ shortcut/SWTOR.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 export PATH="/usr/local/bin:$PATH"
-export LAST_POSSIBLE_OS_TO_RUN_IN=1014
+MACOS_CATALINA=1015
 CURRENT_VERSION=$(sw_vers -productVersion | awk '{print $1}' | sed "s:.[[:digit:]]*.$::g")
-export CURRENT_USER
+export CURRENT_VERSION
 
-if [[ $(echo "${CURRENT_VERSION}" | cut -d"." -f2) -eq 0 ]]; then
-	CURRENT_VERSION_00=$(echo "${CURRENT_VERSION}" | cut -d"." -f1)00
-	export CURRENT_VERSION_00
-	export CURRENT_VERSION=$CURRENT_VERSION_00
+if [[ $(echo "${CURRENT_VERSION}" | cut -d"." -f2 | wc -c) -eq 2 ]]; then
+  CURRENT_VERSION_COMBINED=$(echo "${CURRENT_VERSION}" | cut -d"." -f1)0$(echo "${CURRENT_VERSION}" | cut -d"." -f2)
+  export CURRENT_VERSION_COMBINED
 else
-	CURRENT_VERSION=$(echo "${CURRENT_VERSION}" | cut -d"." -f1)$(echo "${CURRENT_VERSION}" | cut -d"." -f2)
-	export CURRENT_USER
+	CURRENT_VERSION_COMBINED=$(echo "${CURRENT_VERSION}" | cut -d"." -f1)$(echo "${CURRENT_VERSION}" | cut -d"." -f2)
+	export CURRENT_VERSION_COMBINED
 fi
 
 # Oddness for reassigned PATH_TO_SWTOR_LAUNCHER comes from custom/express installation
-if [[ $CURRENT_VERSION -gt $LAST_POSSIBLE_OS_TO_RUN_IN ]]; then
+if [[ CURRENT_VERSION_COMBINED -ge $MACOS_CATALINA ]]; then
 	PREFIX_LOCATION=$(dirname "$(find ~/ -path \*drive_c/Program\ Files/Electronic\ Arts -print -quit 2>/dev/null| tail -1 | grep -o '.*/drive_c')")
 	PATH_TO_SWTOR_LAUNCHER="/drive_c/Program Files/Electronic Arts/BioWare/Star Wars - The Old Republic/launcher.exe"
 	cd "$PREFIX_LOCATION$PATH_TO_SWTOR_LAUNCHER" || PATH_TO_SWTOR_LAUNCHER="/drive_c/Program Files/Electronic Arts/BioWare/Star Wars-The Old Republic/launcher.exe"
