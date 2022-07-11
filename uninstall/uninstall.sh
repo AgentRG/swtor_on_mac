73d0a0039c7a636c40e32f9220b54ffd5460b3fb
AgentRG <ron.gordin@gmail.com>
2022-07-10 18:20:54 -0700

    Updated uninstall script

--- uninstall/uninstall.sh
+++ uninstall/uninstall.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+NONE='\033[00m'
+PURPLE='\033[01;35m'
+
+
+echo -e "${PURPLE}\tAgentRG's SWTOR On Mac\n${NONE}"
+echo -e "${PURPLE}\tDeleting all files related to Wine listed in: wine-cx21.2.0_removal_list.txt\n${NONE}"
+
 cd /
 sudo curl -s https://raw.githubusercontent.com/AgentRG/swtor_on_mac/AgentRG-patch-32/uninstall/wine-cx21.2.0_removal_list.txt | bash
+cd ~
